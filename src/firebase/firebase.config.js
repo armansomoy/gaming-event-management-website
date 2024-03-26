2bdbe9236199be422e1dc5be8b3c461d6a7a5c7a
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2024-03-26 21:15:49 +0600

    file setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxhf5a31NayCzHfvvTbA_xf6MLLosxFXU",
+  authDomain: "gaming-event-management-f7a63.firebaseapp.com",
+  projectId: "gaming-event-management-f7a63",
+  storageBucket: "gaming-event-management-f7a63.appspot.com",
+  messagingSenderId: "738072929756",
+  appId: "1:738072929756:web:23ead59e615ce27d6adb19"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
