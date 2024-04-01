7f298022cced17b8376206f9c593bacb74979b05
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2024-04-01 21:59:06 +0600

    blogs some issue fixed

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -5,12 +5,12 @@ const Blog = ({ blog }) => {
   const { _id, details, title, image_url } = blog;
 
   return (
-    <div className="card card-compact w-full p-6 my-6 bg-base-100 shadow-xl">
+    <div className="card card-compact p-6 bg-base-100 shadow-xl m-6">
       <figure>
         <img src={image_url} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title text-4xl">{title}</h2>
+        <h2 className="card-title text-2xl">{title}</h2>
         {details.length > 150 ? (
           <p className="py-2 text-xl text-[#706F6F]">{details.slice(0, 150)}</p>
         ) : (
--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -1,31 +1,37 @@
-import React from 'react';
-import Navbar from '../../shared/Navbar';
-import Footer from '../../shared/Footer';
-import { useLoaderData } from 'react-router-dom';
-import Blog from '../Blog/Blog';
+import React, { useState } from "react";
+import Navbar from "../../shared/Navbar";
+import Footer from "../../shared/Footer";
+import { useLoaderData } from "react-router-dom";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
-    const news = useLoaderData();
-    return (
-        <div >
-            <Navbar></Navbar>
-            <div>
-            <h2 className='text-6xl font-bold text-center py-24'>Our Blogs</h2>
-            </div>
-            <div className='grid grid-cols-4 container'>
-                
-                <div className="col-span-3">
-                    {
-                        news.map(blog => <Blog key={blog._id} blog={blog}></Blog>)
-                    }
-                </div>
-                <div>
-                    <h2>Sidebar</h2>
-                </div>
-            </div>
-            <Footer></Footer>
+  const news = useLoaderData();
+  const [datalength, setDataLength] = useState(10);
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div>
+        <h2 className="text-6xl font-bold text-center py-24">Our Blogs</h2>
+      </div>
+      <div className="grid grid-cols-4 container">
+        <div className="col-span-3">
+          <div className="grid grid-cols-2 py-6">
+            { news.slice(0, datalength).map((blog) => (
+              <Blog key={blog._id} blog={blog}></Blog>
+            ))}
+          </div>
+          <div className={datalength === news.length ? 'hidden' : 'py-2 px-4 my-6 text-center'}>
+            <button className="btn btn-primary" onClick={() => {setDataLength(news.length)}}>Sell All Blogs</button>
+          </div>
         </div>
-    );
+        <div>
+          <h2>Sidebar</h2>
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Blogs;
